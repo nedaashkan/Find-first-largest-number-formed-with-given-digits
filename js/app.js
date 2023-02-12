d9c631c35352b2ff808d0aae2dd5db7c1583a68e
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-02-13 01:53:31 +0330

    i wrote the javascript

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchButton);
+let resultEl = document.getElementById("result-el");
+let inputEl = document.getElementById("input-el");
+let value = inputEl.value;
+let valueArray = Array.from(value);
+let arrayBiggerNumberThenFistNumber = [];
+let firstBiggerNumberThenFistNumber;
+let result;
 function searchButton(event) {
   event.preventDefault();
-  console.log("hi")
+  for (let i = 0; i < valueArray.length; i++) {
+    if (valueArray[i] > valueArray[0]) {
+      arrayBiggerNumberThenFistNumber.push(valueArray[i]);
+    }
+  }
+  arrayBiggerNumberThenFistNumber.sort((a, b) => a - b);
+  valueArray.sort((a, b) => a - b);
+  firstBiggerNumberThenFistNumber = arrayBiggerNumberThenFistNumber[0];
+  if (firstBiggerNumberThenFistNumber === undefined) {
+    result = "it doesn't exist";
+  } else {
+    result = firstBiggerNumberThenFistNumber;
+  }
+  resultEl.textContent = result;
+  arrayBiggerNumberThenFistNumber = [];
 }
+// sort the valueArray Array and delete the firstBiggerNumberThenFistNumber value from the sort the valueArray Array
+`find this number ${firstBiggerNumberThenFistNumber} from valueArray array delete the first number you find`;
