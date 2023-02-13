062140233b8f28ae6c9c536b05404a5ae665c88a
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-02-13 09:58:09 +0330

    deleted the firstBiggerNumberThenFistNumber from sorted valueArray and showing result

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchButton);
 let resultEl = document.getElementById("result-el");
 let inputEl = document.getElementById("input-el");
-let value = inputEl.value;
-let valueArray = Array.from(value);
 let arrayBiggerNumberThenFistNumber = [];
 let firstBiggerNumberThenFistNumber;
 let result;
 function searchButton(event) {
   event.preventDefault();
+  let value = inputEl.value;
+  let valueArray = Array.from(value);
   for (let i = 0; i < valueArray.length; i++) {
     if (valueArray[i] > valueArray[0]) {
       arrayBiggerNumberThenFistNumber.push(valueArray[i]);
@@ -17,13 +17,22 @@ function searchButton(event) {
   arrayBiggerNumberThenFistNumber.sort((a, b) => a - b);
   valueArray.sort((a, b) => a - b);
   firstBiggerNumberThenFistNumber = arrayBiggerNumberThenFistNumber[0];
+  let index = valueArray.indexOf(firstBiggerNumberThenFistNumber);
+  valueArray.splice(index, 1);
   if (firstBiggerNumberThenFistNumber === undefined) {
     result = "it doesn't exist";
   } else {
-    result = firstBiggerNumberThenFistNumber;
+    result = firstBiggerNumberThenFistNumber + `${valueArray.join("")}`;
   }
   resultEl.textContent = result;
   arrayBiggerNumberThenFistNumber = [];
 }
-// sort the valueArray Array and delete the firstBiggerNumberThenFistNumber value from the sort the valueArray Array
-`find this number ${firstBiggerNumberThenFistNumber} from valueArray array delete the first number you find`;
+// let array = [2,5,5,5,6,9,4,3,3,3,4,4]
+// 255569433344
+// let sort = array.sort((a, b) => a - b);
+// let number = 3
+// console.log(number);
+// let index = sort.indexOf(number);
+//  sort.splice(index, 1);
+// let answer = number + `${sort.join("")}`
+// let realAnswer = 323344455569
